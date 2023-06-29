1eae2fa42fb1da6163e8072c93452aa9a3948127
Steve <34465153+nicelizhi@users.noreply.github.com>
2023-06-29 22:50:02 +0800

    Update Editable.php
    add editlabe reload data

--- src/Grid/Displayers/Editable.php
+++ src/Grid/Displayers/Editable.php
@@ -192,6 +192,9 @@ class Editable extends AbstractDisplayer
         } else {
             $.admin.toastr.error(response.message, '', {positionClass:"toast-top-center"});
         }
+        if(response.refresh==true) {
+            $.admin.reload();
+        }
     }
 }
 STR;
